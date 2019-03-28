d0eb232f7ee89af68e15e3ae6d0faeaf3d989565
whatsong <tommy.ge.andrew@gmail.com>
2019-03-28 21:21:47 +1000

    Commit

--- whatsong v3/Controllers/Movies/MoviesHorizontalController.swift
+++ whatsong v3/Controllers/Movies/MoviesHorizontalController.swift
@@ -8,36 +8,50 @@
 
 import UIKit
 
-class MoviesHorizontalController: BaseCvController, UICollectionViewDelegateFlowLayout  {
+class MoviesHorizontalController: HorizontalSnappingController, UICollectionViewDelegateFlowLayout  {
     
     let cellId = "cellId"
+    var latestMoviesArray: LatestMovies?
+    
+    var didSelectHandler: ((LatestMovie) -> ())?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .blue
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
-        
-        if let layout = collectionViewLayout as? UICollectionViewFlowLayout    {
-            layout.scrollDirection = .horizontal
-        }
+        collectionView.register(MoviesRowCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.backgroundColor = .white
         
     }
     
+    let topBottomSpacing: CGFloat = 12
+    let lineSpacing: CGFloat = 12
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return latestMoviesArray?.data.count ?? 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MoviesRowCell
+        let movie = latestMoviesArray?.data[indexPath.item]
+        cell.titleLabel.text = movie?.title
+        cell.yearLabel.text = movie?.year
+        let urlPrefix = "https://www.what-song.com"
+        let urlSuffix = movie?.poster ?? ""
+        let url = URL(string: urlPrefix + urlSuffix)
+        cell.imageView.sd_setImage(with: url)
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: (view.frame.height - 12) / 2)
+        return CGSize(width: view.frame.width * 0.85, height: (view.frame.height - topBottomSpacing * 2 - lineSpacing * 2) / 3)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        return UIEdgeInsets(top: topBottomSpacing, left: 20, bottom: topBottomSpacing, right: 40)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let movie = latestMoviesArray?.data[indexPath.item]  {
+            didSelectHandler?(movie)
+        }
     }
 }
---
+++ whatsong v3/Helpers/VerticalStackView.swift
@@ -0,0 +1,25 @@
+//
+//  VerticalStackView.swift
+//  whatsong v3
+//
+//  Created by Tom Andrew on 7/3/19.
+//  Copyright © 2019 Tom Andrew. All rights reserved.
+//
+
+import UIKit
+
+class VerticalStackView: UIStackView {
+    
+    init(arrangedSubviews: [UIView], spacing: CGFloat = 0) {
+        super.init(frame: .zero)
+        
+        arrangedSubviews.forEach({addArrangedSubview($0)})
+        
+        self.spacing = spacing
+        self.axis = .vertical
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
